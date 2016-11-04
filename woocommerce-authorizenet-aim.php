d6bd7226ab172bace34cec88336933f10304cd1a
snogcel <ledniknoj@gmail.com>
2016-11-03 21:37:12 -0600

    initial dashification

--- woocommerce-authorizenet-aim.php
+++ woocommerce-authorizenet-aim.php
@@ -25,7 +25,7 @@ class SPYR_AuthorizeNet_AIM extends WC_Payment_Gateway {
 		$this->has_fields = true;
 
 		// Supports the default credit card form
-		$this->supports = array( 'default_credit_card_form' );
+		// $this->supports = array( 'default_credit_card_form' );
 
 		// This basically defines your settings which are then loaded with init_settings()
 		$this->init_form_fields();
@@ -65,24 +65,24 @@ class SPYR_AuthorizeNet_AIM extends WC_Payment_Gateway {
 				'title'		=> __( 'Title', 'spyr-authorizenet-aim' ),
 				'type'		=> 'text',
 				'desc_tip'	=> __( 'Payment title the customer will see during the checkout process.', 'spyr-authorizenet-aim' ),
-				'default'	=> __( 'Credit card', 'spyr-authorizenet-aim' ),
+				'default'	=> __( 'Dash', 'spyr-authorizenet-aim' ),
 			),
 			'description' => array(
 				'title'		=> __( 'Description', 'spyr-authorizenet-aim' ),
 				'type'		=> 'textarea',
 				'desc_tip'	=> __( 'Payment description the customer will see during the checkout process.', 'spyr-authorizenet-aim' ),
-				'default'	=> __( 'Pay securely using your credit card.', 'spyr-authorizenet-aim' ),
+				'default'	=> __( 'Pay securely using Dash.', 'spyr-authorizenet-aim' ),
 				'css'		=> 'max-width:350px;'
 			),
 			'api_login' => array(
-				'title'		=> __( 'Authorize.net API Login', 'spyr-authorizenet-aim' ),
+				'title'		=> __( 'Dashpay API Key', 'spyr-authorizenet-aim' ),
 				'type'		=> 'text',
-				'desc_tip'	=> __( 'This is the API Login provided by Authorize.net when you signed up for an account.', 'spyr-authorizenet-aim' ),
+				'desc_tip'	=> __( 'This is the API Key provided by the Dash Payment Service when you signed up for an account.', 'spyr-authorizenet-aim' ),
 			),
 			'trans_key' => array(
-				'title'		=> __( 'Authorize.net Transaction Key', 'spyr-authorizenet-aim' ),
+				'title'		=> __( 'Dashpay User Account', 'spyr-authorizenet-aim' ),
 				'type'		=> 'password',
-				'desc_tip'	=> __( 'This is the Transaction Key provided by Authorize.net when you signed up for an account.', 'spyr-authorizenet-aim' ),
+				'desc_tip'	=> __( 'This is an arbitrary user account presently.', 'spyr-authorizenet-aim' ),
 			),
 			'environment' => array(
 				'title'		=> __( 'Authorize.net Test Mode', 'spyr-authorizenet-aim' ),
@@ -107,57 +107,70 @@ class SPYR_AuthorizeNet_AIM extends WC_Payment_Gateway {
 
 		// Decide which URL to post to
 		$environment_url = ( "FALSE" == $environment ) 
-						   ? 'https://secure.authorize.net/gateway/transact.dll'
-						   : 'https://test.authorize.net/gateway/transact.dll';
+						   ? 'https://dev-test.dash.org/dash-payment-service/createReceiver'
+						   : 'https://dev-test.dash.org/dash-payment-service/createReceiver';
 
 		// This is where the fun stuff begins
 		$payload = array(
+		    // Dashpay API Key and Credentials
+		    "api_key"              	=> $this->api_login,
+		    "email"                	=> $this->trans_key,
+
+		    // Order Details
+		    "currency"              => "USD",
+		    "amount"             	=> $customer_order->order_total,
+		    "description"        	=> str_replace( "#", "", $customer_order->get_order_number() ),
+
+		    // Callback URL
+		    "callbackUrl"           => "https://dev-test.dash.org:3001/dash-payment-service/cb"
+
+
 			// Authorize.net Credentials and API Info
-			"x_tran_key"           	=> $this->trans_key,
-			"x_login"              	=> $this->api_login,
-			"x_version"            	=> "3.1",
+			//"x_tran_key"           	=> $this->trans_key,
+			//"x_login"              	=> $this->api_login,
+			//"x_version"            	=> "3.1",
 			
 			// Order total
-			"x_amount"             	=> $customer_order->order_total,
+			//"x_amount"             	=> $customer_order->order_total,
 			
 			// Credit Card Information
-			"x_card_num"           	=> str_replace( array(' ', '-' ), '', $_POST['spyr_authorizenet_aim-card-number'] ),
-			"x_card_code"          	=> ( isset( $_POST['spyr_authorizenet_aim-card-cvc'] ) ) ? $_POST['spyr_authorizenet_aim-card-cvc'] : '',
-			"x_exp_date"           	=> str_replace( array( '/', ' '), '', $_POST['spyr_authorizenet_aim-card-expiry'] ),
+			//"x_card_num"           	=> str_replace( array(' ', '-' ), '', $_POST['spyr_authorizenet_aim-card-number'] ),
+			//"x_card_code"          	=> ( isset( $_POST['spyr_authorizenet_aim-card-cvc'] ) ) ? $_POST['spyr_authorizenet_aim-card-cvc'] : '',
+			//"x_exp_date"           	=> str_replace( array( '/', ' '), '', $_POST['spyr_authorizenet_aim-card-expiry'] ),
 			
-			"x_type"               	=> 'AUTH_CAPTURE',
-			"x_invoice_num"        	=> str_replace( "#", "", $customer_order->get_order_number() ),
-			"x_test_request"       	=> $environment,
-			"x_delim_char"         	=> '|',
-			"x_encap_char"         	=> '',
-			"x_delim_data"         	=> "TRUE",
-			"x_relay_response"     	=> "FALSE",
-			"x_method"             	=> "CC",
+			//"x_type"               	=> 'AUTH_CAPTURE',
+			//"x_invoice_num"        	=> str_replace( "#", "", $customer_order->get_order_number() ),
+			//"x_test_request"       	=> $environment,
+			//"x_delim_char"         	=> '|',
+			//"x_encap_char"         	=> '',
+			//"x_delim_data"         	=> "TRUE",
+			//"x_relay_response"     	=> "FALSE",
+			//"x_method"             	=> "CC",
 			
 			// Billing Information
-			"x_first_name"         	=> $customer_order->billing_first_name,
-			"x_last_name"          	=> $customer_order->billing_last_name,
-			"x_address"            	=> $customer_order->billing_address_1,
-			"x_city"              	=> $customer_order->billing_city,
-			"x_state"              	=> $customer_order->billing_state,
-			"x_zip"                	=> $customer_order->billing_postcode,
-			"x_country"            	=> $customer_order->billing_country,
-			"x_phone"              	=> $customer_order->billing_phone,
-			"x_email"              	=> $customer_order->billing_email,
+			//"x_first_name"         	=> $customer_order->billing_first_name,
+			//"x_last_name"          	=> $customer_order->billing_last_name,
+			//"x_address"            	=> $customer_order->billing_address_1,
+			//"x_city"              	=> $customer_order->billing_city,
+			//"x_state"              	=> $customer_order->billing_state,
+			//"x_zip"                	=> $customer_order->billing_postcode,
+			//"x_country"            	=> $customer_order->billing_country,
+			//"x_phone"              	=> $customer_order->billing_phone,
+			//"x_email"              	=> $customer_order->billing_email,
 			
 			// Shipping Information
-			"x_ship_to_first_name" 	=> $customer_order->shipping_first_name,
-			"x_ship_to_last_name"  	=> $customer_order->shipping_last_name,
-			"x_ship_to_company"    	=> $customer_order->shipping_company,
-			"x_ship_to_address"    	=> $customer_order->shipping_address_1,
-			"x_ship_to_city"       	=> $customer_order->shipping_city,
-			"x_ship_to_country"    	=> $customer_order->shipping_country,
-			"x_ship_to_state"      	=> $customer_order->shipping_state,
-			"x_ship_to_zip"        	=> $customer_order->shipping_postcode,
+			//"x_ship_to_first_name" 	=> $customer_order->shipping_first_name,
+			//"x_ship_to_last_name"  	=> $customer_order->shipping_last_name,
+			//"x_ship_to_company"    	=> $customer_order->shipping_company,
+			//"x_ship_to_address"    	=> $customer_order->shipping_address_1,
+			//"x_ship_to_city"       	=> $customer_order->shipping_city,
+			//"x_ship_to_country"    	=> $customer_order->shipping_country,
+			//"x_ship_to_state"      	=> $customer_order->shipping_state,
+			//"x_ship_to_zip"        	=> $customer_order->shipping_postcode,
 			
 			// Some Customer Information
-			"x_cust_id"            	=> $customer_order->user_id,
-			"x_customer_ip"        	=> $_SERVER['REMOTE_ADDR'],
+			//"x_cust_id"            	=> $customer_order->user_id,
+			//"x_customer_ip"        	=> $_SERVER['REMOTE_ADDR'],
 			
 		);
 	
@@ -183,36 +196,51 @@ class SPYR_AuthorizeNet_AIM extends WC_Payment_Gateway {
 			$resp = explode( "|", $line );
 		}
 
+        // Payment has been successful
+        $customer_order->add_order_note( __( 'Authorize.net payment completed.', 'spyr-authorizenet-aim' ) );
+
+        // Mark order as Paid
+        $customer_order->payment_complete();
+
+        // Empty the cart (Very important step)
+        $woocommerce->cart->empty_cart();
+
+        // Redirect to thank you page
+        return array(
+            'result'   => 'success',
+            'redirect' => $this->get_return_url( $customer_order ),
+        );
+
 		// Get the values we need
-		$r['response_code']             = $resp[0];
-		$r['response_sub_code']         = $resp[1];
-		$r['response_reason_code']      = $resp[2];
-		$r['response_reason_text']      = $resp[3];
+		// $r['response_code']             = $resp[0];
+		// $r['response_sub_code']         = $resp[1];
+		// $r['response_reason_code']      = $resp[2];
+		// $r['response_reason_text']      = $resp[3];
 
 		// Test the code to know if the transaction went through or not.
 		// 1 or 4 means the transaction was a success
-		if ( ( $r['response_code'] == 1 ) || ( $r['response_code'] == 4 ) ) {
+		// if ( ( $r['response_code'] == 1 ) || ( $r['response_code'] == 4 ) ) {
 			// Payment has been successful
-			$customer_order->add_order_note( __( 'Authorize.net payment completed.', 'spyr-authorizenet-aim' ) );
+		//	$customer_order->add_order_note( __( 'Authorize.net payment completed.', 'spyr-authorizenet-aim' ) );
 												 
 			// Mark order as Paid
-			$customer_order->payment_complete();
+		//	$customer_order->payment_complete();
 
 			// Empty the cart (Very important step)
-			$woocommerce->cart->empty_cart();
+		//	$woocommerce->cart->empty_cart();
 
 			// Redirect to thank you page
-			return array(
-				'result'   => 'success',
-				'redirect' => $this->get_return_url( $customer_order ),
-			);
-		} else {
+		//	return array(
+		//		'result'   => 'success',
+		//		'redirect' => $this->get_return_url( $customer_order ),
+		//	);
+		// } else {
 			// Transaction was not succesful
 			// Add notice to the cart
-			wc_add_notice( $r['response_reason_text'], 'error' );
+		//	wc_add_notice( $r['response_reason_text'], 'error' );
 			// Add note to the order for your reference
-			$customer_order->add_order_note( 'Error: '. $r['response_reason_text'] );
-		}
+		//	$customer_order->add_order_note( 'Error: '. $r['response_reason_text'] );
+		// }
 
 	}
 	
